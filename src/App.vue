f7a1bf2dd5d9276472ad1e0577e2041a2719fa55
Tsubasa Masaki <tsubasa.masaki@medpeer.co.jp>
2023-07-09 12:05:15 +0900

    step12

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,11 @@
 <script setup>
-  import { ref, watch } from 'vue'
+import { ref } from 'vue'
+import ChildComp from './ChildComp.vue'
 
-const todoId = ref(1)
-const todoData = ref(null)
 
-async function fetchData() {
-  todoData.value = null
-  const res = await fetch(
-    `https://jsonplaceholder.typicode.com/todos/${todoId.value}`
-  )
-  todoData.value = await res.json()
-}
-fetchData()
-
-watch(todoId, fetchData)
+const greeting = ref('Hello from parent')
 </script>
 
 <template>
-  <p>Todo id: {{ todoId }}</p>
-  <button @click="todoId++">Fetch next todo</button>
-  <p v-if="!todoData">Loading...</p>
-  <pre v-else>{{ todoData }}</pre>
+  <ChildComp :msg="greeting"/>
 </template>
